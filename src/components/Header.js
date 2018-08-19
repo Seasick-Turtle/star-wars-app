026d261875ee5445e584c6f3b3fd58ea9b981973
Christopher Martin <martincdev@gmail.com>
2018-08-19 17:02:02 -0500

    add logo and basic header css styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import SearchBox from '../containers/SearchBox';
 import Themes from './Themes';
+import '../styles/Header.css';
+import StarWarsLogo from './star_wars_logo_PNG30.png';
 
 const Header = () => {
   return (
-    <div>
-      <p>STAR WARS</p>
-      <SearchBox />
-      <Themes />
-    </div>
+    <header className='main-header'>
+        <img src={StarWarsLogo} alt='gold star wars logo' className='logo'/>
+        <SearchBox />
+        <Themes />
+    </header>
   )
 };
 
