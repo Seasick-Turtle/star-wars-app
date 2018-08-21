6d12e6058dcb6ad5274c0268b85e62e5d178485a
Christopher Martin <martincdev@gmail.com>
2018-08-20 23:40:54 -0500

    add more styling to search bar and button

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -5,14 +5,15 @@ export default class Search extends Component {
   render() {
     return (
       <div className='search-main'>
-
+        {/*<i className='search-icon'></i>*/}
        <input type='search' placeholder='Search for planets, etc.' id='search-main__bar' />
-        <button className='search-main__button'>Search</button>
+
+        <button className='search-main__search-button'>Search</button>
         <div>
           Filter By:
-          <button>Planets</button>
-          <button>Characters</button>
-          <button>Movies</button>
+          <button className='search-main__filter-button'>Planets</button>
+          <button className='search-main__filter-button'>Characters</button>
+          <button className='search-main__filter-button'>Movies</button>
         </div>
 
       </div>
