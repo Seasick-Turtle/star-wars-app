94b083363f6d7130ed7b34f4aa25a8bfd156e473
Christopher Martin <martincdev@gmail.com>
2018-10-10 22:14:51 -0500

    fixed a tag descendant errors

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -15,19 +15,16 @@ export default class MenuItem extends Component {
     const { film } = this.props;
 
     return (
-      <React.Fragment>
-        <a className='menuItem__main--dropdown filmTitle' href='#'>
-          {film.title}
-          <div className='menuItem__main--content'>
-            <a href='#'>People</a>
-            <a href='#'>Planets</a>
-            <a href='#'>Species</a>
-            <a href='#'>Starships</a>
-            <a href='#'>Vehicles</a>
-          </div>
-
-        </a>
-      </React.Fragment>
+      <div className='menuItem__main--dropdown filmTitle'>
+        {film.title}
+        <div className='menuItem__main--content'>
+          <a href='#'>People</a>
+          <a href='#'>Planets</a>
+          <a href='#'>Species</a>
+          <a href='#'>Starships</a>
+          <a href='#'>Vehicles</a>
+        </div>
+      </div>
     )
   }
 
