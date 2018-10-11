136e6ffd581f1fc94482201eba46d23a54e49f08
Christopher Martin <martincdev@gmail.com>
2018-10-10 22:05:49 -0500

    add top level dropdown menus

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/MenuItem.css';
 
-const MenuItem  = ({ film }) => {
-  return (
-    <React.Fragment>
-      <a className='filmTitle' href='#'>{film.title}</a>
-    </React.Fragment>
-  )
-};
+export default class MenuItem extends Component {
+  constructor(props) {
+    super(props);
 
-export default MenuItem;
+    this.state = {
+      showMenu: false
+    }
+  }
+
+
+  render() {
+    const { film } = this.props;
+
+    return (
+      <React.Fragment>
+        <a className='menuItem__main--dropdown filmTitle' href='#'>
+          {film.title}
+          <div className='menuItem__main--content'>
+            <a href='#'>People</a>
+            <a href='#'>Planets</a>
+            <a href='#'>Species</a>
+            <a href='#'>Starships</a>
+            <a href='#'>Vehicles</a>
+          </div>
+
+        </a>
+      </React.Fragment>
+    )
+  }
+
+};
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,7 +16,7 @@ import '../styles/Nav.css';
 const Nav = () => {
   return (
     <nav className='nav'>
-      <div className='nav__item nav__item--collapsible-menu'>
+      <ul className='nav__item nav__item--collapsible-menu'>
         <li className='nav__items'><MenuItem film={TPM}/></li>
         <li className='nav__items'><MenuItem film={AOTC}/></li>
         <li className='nav__items'><MenuItem film={ROTS}/></li>
@@ -24,8 +24,7 @@ const Nav = () => {
         <li className='nav__items'><MenuItem film={TESB}/></li>
         <li className='nav__items'><MenuItem film={ROTJ}/></li>
         <li className='nav__items'><MenuItem film={TFA}/></li>
-
-      </div>
+      </ul>
       <Search />
     </nav>
 
