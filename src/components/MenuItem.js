dbd548dfb48d68491971b6593f45a05e2c19a6fc
Christopher Martin <martincdev@gmail.com>
2018-10-16 14:36:35 -0500

    create functions to handle displaying and limiting items for nested dropdown menus

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -11,13 +11,41 @@ export default class MenuItem extends Component {
   }
 
   displayItems = (film, category ) => {
+    if (film[category].length > 10) {
+      return (
+        <React.Fragment>
+          {this.limitItems(film, category)}
+          <a href='#'>See More</a>
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <li className='menuItem__submenu-content'>{film[category].map((item) => {
+          {
+            if (film[category].length < 11) {
+              return <a className='menuItem__submenu-item' key={item.id} href='#'>{item.name}</a>;
+            }
+          }
+        })}</li>
+      )
+    }
+  };
+
+  limitItems = (film, category ) => {
     return (
       <li className='menuItem__submenu-content'>{film[category].map((item) => {
-        return <a className='menuItem__submenu-item' key={item.id} href='#'>{item.name}</a>
+        {
+          if (film[category].length < 11) {
+            return <a className='menuItem__submenu-item' key={item.id} href='#'>{item.name}</a>;
+          } else {
+            film[category].splice(10).map((item) => {
+              return <a className='menuItem__submenu-item' key={item.id} href='#'>{item.name}</a>;
+            });
+          }
+        }
       })}</li>
     )
   };
-
 
   render() {
     const { film } = this.props;
@@ -57,9 +85,9 @@ export default class MenuItem extends Component {
              (
               <li className='menuItem__main-item'>
                 <a href='#'>Vehicles</a>
-              <ul className='menuItem__submenu'>
-                {this.displayItems(film, 'vehicles')}
-              </ul>
+                <ul className='menuItem__submenu'>
+                  {this.displayItems(film, 'vehicles')}
+                </ul>
               </li>
             ))
           }
