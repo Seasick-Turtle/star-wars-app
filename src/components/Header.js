b7064401631e20b7bebc670b40b73705006bf10a
Christopher Martin <martincdev@gmail.com>
2019-04-01 19:33:15 -0500

    add link to logo to allow users to travel back to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Nav from '../components/Nav';
 import StarWarsLogo from '../styles/swapi-example-logo-transparent.png';
 
 const Header = () => {
   return (
     <header className='main-header'>
+      <Link to='/'>
         <img src={StarWarsLogo} alt='gold star wars logo' className='logo'/>
+      </Link>
       <Nav />
     </header>
   )
