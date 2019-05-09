a8e8733dba50c72ab9e360b427f19d4e3d41cba7
Christopher Martin <martincdev@gmail.com>
2019-05-09 16:12:32 -0500

    Remove custom star wars logo to avoid legal issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Nav from '../components/Nav';
-import StarWarsLogo from '../styles/swapi-example-logo-transparent.png';
 
 const Header = () => {
   return (
     <header className='main-header'>
       <Link to='/'>
-        <img src={StarWarsLogo} alt='gold star wars logo' className='logo'/>
+        <h2 className='header-title'>Star Wars API Example</h2>
       </Link>
       <Nav />
     </header>
