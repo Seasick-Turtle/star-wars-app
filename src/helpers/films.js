ee5a69d4ba45e70c70874a0977d9ac5add43d754
Christopher Martin <martincdev@gmail.com>
2020-10-31 22:08:40 -0500

    Added images and styling for images

---
+++ src/helpers/films.js
@@ -0,0 +1,17 @@
+import ThePhantomMenace from '../images/The Phantom Menace.jpg';
+import AttackOfTheClones from '../images/Attack of the Clones.jpg';
+import RevengeOfTheSith from '../images/Revenge of the Sith.jpg';
+import ANewHope from '../images/A New Hope.jpg'
+import TheEmpireStrikesBack from '../images/The Empire Strikes Back.jpg';
+import ReturnOfTheJedi from '../images/Return of the Jedi.jpg';
+import TheForceAwakens from '../images/The Force Awakens.jpg';
+
+export default {
+  ThePhantomMenace,
+  AttackOfTheClones,
+  RevengeOfTheSith,
+  ANewHope,
+  TheEmpireStrikesBack,
+  ReturnOfTheJedi,
+  TheForceAwakens
+}
