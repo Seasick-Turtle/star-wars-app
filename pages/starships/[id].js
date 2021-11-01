5b259de4d902187b04f00983b2a6d441bfbeeca3
Christopher Martin <martincdev@tuta.io>
2021-10-31 23:54:54 -0500

    Refactored to use graphql instead of fetching

--- pages/starships/[id].js
+++ pages/starships/[id].js
@@ -1,66 +1,99 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { conditionalRenderData } from '../../helpers/conditionalRenderData';
-import { loadState } from '../../localStorage';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_STARSHIP = gql`
+	query starship($starshipId: ID) {
+		starship(starshipID: $starshipId) {
+			name
+			model
+			manufacturers
+			costInCredits
+			crew
+			passengers
+			starshipClass
+			hyperdriveRating
+			cargoCapacity
+			consumables
+		}
+	}
+`;
 
 const People = () => {
-	const data = loadState('starships');
 	const {
-		name,
-		crew,
-		passengers,
-		model,
-		consumables,
-		cargo_capacity,
-		hyperdrive_rating,
-		manufacturer,
-		starship_class,
-		cost_in_credits,
-	} = data;
+		query: { id: starshipId },
+	} = useRouter();
 
-	return (
-		<main>
-			<section className="categories__component">
-				<Link href="/">Back</Link>
-				<h2 className="categories__title">{name}</h2>
-				<p>
-					<strong>Crew: </strong>
-					{conditionalRenderData(crew, '')}
-				</p>
-				<p>
-					<strong>Passengers: </strong>
-					{conditionalRenderData(passengers, '')}
-				</p>
-				<p>
-					<strong>Model: </strong>
-					{model}
-				</p>
-				<p>
-					<strong>Consumables: </strong>
-					{conditionalRenderData(consumables, '')}
-				</p>
-				<p>
-					<strong>Cargo Capacity: </strong>
-					{conditionalRenderData(cargo_capacity, 'tons')}
-				</p>
-				<p>
-					<strong>Hyperdrive Rating: </strong>
-					{hyperdrive_rating}
-				</p>
-				<p>
-					<strong>Manufacturer: </strong>
-					{manufacturer}
-				</p>
-				<p>
-					<strong>Starship Class: </strong>
-					{starship_class}
-				</p>
-				<p>
-					<strong>Cost (In Credits): </strong>
-					{conditionalRenderData(cost_in_credits, '')}
-				</p>
-			</section>
-		</main>
-	);
+	const { loading, error, data } = useQuery(GET_STARSHIP, {
+		variables: { starshipId },
+		errorPolicy: 'all',
+		fetchPolicy: 'cache-first',
+	});
+
+	if (loading) {
+		return 'Loading...';
+	} else {
+		const {
+			starship: {
+				name,
+				model,
+				manufacturers,
+				costInCredits,
+				crew,
+				passengers,
+				starshipClass,
+				hyperdriveRating,
+				cargoCapacity,
+				consumables,
+			},
+		} = data;
+
+		return (
+			<main>
+				<section className="categories__component">
+					<Link href="/">Back</Link>
+					<h2 className="categories__title">{name}</h2>
+					<p>
+						<strong>Crew: </strong>
+						{conditionalRenderData(crew, '')}
+					</p>
+					<p>
+						<strong>Passengers: </strong>
+						{conditionalRenderData(passengers, '')}
+					</p>
+					<p>
+						<strong>Model: </strong>
+						{model}
+					</p>
+					<p>
+						<strong>Consumables: </strong>
+						{conditionalRenderData(consumables, '')}
+					</p>
+					<p>
+						<strong>Cargo Capacity: </strong>
+						{conditionalRenderData(cargoCapacity, 'tons')}
+					</p>
+					<p>
+						<strong>Hyperdrive Rating: </strong>
+						{hyperdriveRating}
+					</p>
+					<p>
+						<strong>Manufacturer: </strong>
+						{manufacturers}
+					</p>
+					<p>
+						<strong>Starship Class: </strong>
+						{starshipClass}
+					</p>
+					<p>
+						<strong>Cost (In Credits): </strong>
+						{conditionalRenderData(costInCredits, '')}
+					</p>
+				</section>
+			</main>
+		);
+	}
 };
 
 export default People;
